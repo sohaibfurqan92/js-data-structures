224182ec860a97c64b1227540e0b0a93a0866d1e
Sohaib Furqan <sfurqan@asaadshub.com>
2023-02-28 22:47:41 +0000

    Adding HashTable implementation in JS

---
+++ HashTable.js
@@ -0,0 +1,56 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  get(key) {
+    let address = this._hash(key);
+    let currentBucket = this.data[address];
+    if (currentBucket) {
+      for (let i = 0; i < currentBucket.length; i++) {
+        if (currentBucket[i][0] === key) {
+          return currentBucket[i][1];
+        }
+      }
+    }
+    return undefined;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+
+    if (!this.data[address]) {
+      this.data[address] = [];
+    }
+
+    this.data[address].push([key, value]);
+  }
+
+  keys() {
+    const keysArray = [];
+    for (let i = 0; i < this.data.length; i++) {
+      if (this.data[i]) {
+        keysArray.push(this.data[i][0][0]);
+      }
+    }
+    return keysArray;
+  }
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set('grapes', 10000);
+myHashTable.set('apples', 9);
+myHashTable.set('oranges', 2);
+// myHashTable.keys()
+console.log(myHashTable.keys());
+
+// myHashTable.get('apples');
+// console.log(myHashTable);
